edb08d875a93822e7bb3285bf250a8db157fdcda
jaazmeyn <jasmin.blanda@gmail.com>
2021-02-19 00:03:27 +0100

    new js

--- script/main.js
+++ script/main.js
@@ -1,19 +1,19 @@
 class Warenkorb{
     constructor(){
-        this.warenkorbProdukte = [];//alles eingekauften 
+        this.warenkorbProdukte = []; //bestimmte Sortimentauswahl von Kunde
     }
-    warenkorbHinzufuegen(produkt){
+    hinzufuegen(produkt){
         console.log(this.warenkorbProdukte, 'warenkorbprodukte')
-        //console.log('hinzufügen: ' + this.produktVorhanden(produkt.id))//geht nicht
-        if(this.produktVorhanden(produkt.id)){
+        //console.log('hinzufügen: ' + this.vorhanden(produkt.id))//geht nicht
+        if(this.vorhanden(produkt.id)){
             console.log(" produkt vorhanden true");
         } else {
             console.log(" produkt vorhanden false");
             this.warenkorbProdukte.push(produkt);//id steckt drinnen
             $('#warenkorb').show();
-        }//warenkorb.warenkorbAnzeigen();//aktualiesieren
+        }//warenkorb.anzeigen();//aktualiesieren
     }
-    warenkorbAnzeigen(){
+    anzeigen(){
         $('#warenkorb').html(''); console.log('warenkorb anzeigen')
         let summegesamt=0;
         this.warenkorbProdukte.forEach(p => {
@@ -22,7 +22,7 @@ class Warenkorb{
         });
         $('#warenkorb').append("<div><hr>"+summegesamt+"€</div>")
     }
-    produktVorhanden(id){ 
+    vorhanden(id){ //produkt vorhanden?
             console.log('produkte',this.warenkorbProdukte, );
         let vorhanden = false;
         this.warenkorbProdukte.forEach(p => {
@@ -33,7 +33,7 @@ class Warenkorb{
                     console.log(p);
                 p.warenkorbanzahl++;
                 // summe = anzahl mal preis
-                p.createProduct();
+                p.create();
                     console.log(p);
                 return;
             }
@@ -48,8 +48,6 @@ class Warenkorb{
         };
     }
 }
-let warenkorb = new Warenkorb();
-
 
 class Produkt {
     constructor(productname, preis, img, id){
@@ -59,9 +57,9 @@ class Produkt {
         this.img = img;
         this.html;
         this.warenkorbanzahl = 1;
-        this.createProduct(productname, preis, id); //aufruf
+        this.create(productname, preis, id); //aufruf
     }
-    createProduct(){
+    create(){
         this.html = `
             <div id="${this.id}"><h3>${this.productname}</h3>
             <img src="img/item${this.img}.jpg" alt="bild">
@@ -74,31 +72,32 @@ class Produkt {
     }
     addButton(){// produkte im array vom sortiment an der stelle(id)
         console.log('this',this); //funktionsaufruf auf dem objekt mitgegebenen im html
-        warenkorb.warenkorbHinzufuegen(this);
-        warenkorb.warenkorbAnzeigen();
+        warenkorb.hinzufuegen(this);
+        warenkorb.anzeigen();
     }
 }
-
 
 class Sortiment { //alle produkte für die darstellung
     constructor(){            
     }
-    produkte = [];
-    sortimentHinzufuegen(uebergebeneProdukte){//jedes einzelne erstellte erst nach erstellen möglich
+    produkte = [ ];
+    hinzufuegen(uebergebeneProdukte){//jedes einzelne erstellte erst nach erstellen möglich
         uebergebeneProdukte.forEach(p=>{ //einzelnes produkt im sortimentarray
             this.produkte.push(p);
         });            
     }
-    sortimentAnzeigen(){
+    anzeigen(){
         $('#sortiment').html(''); //zuerst leeren damit nicht doppelt gepusht
-        this.produkte.forEach(p => {// im p ein objekt aus produktearay
+        this.produkte.forEach(p => {// im p ein objekt aus produktearray
             $('#sortiment').append(p.html); //neues produkt html aus konstruktor
         });    
     }       
 }
 
+//Programmablauf
 
-//erstellen produkt (html, onclick add to warenkorb, wenn noch nicht vorhanden, sonst anzahl erhöhen. sub)
+//Produkt aus Class( darin definiert: html, onclick add to Warenkorb -> wenn noch nicht vorhanden, sonst Anzahl erhöhen. sub)
+
 let p1 = new Produkt('iphone12 Pro', 300, 1, 0);
 let p2 = new Produkt('iphone12', 760, 2, 1);
 let p3 = new Produkt('iphone12 mini', 760, 3, 2);
@@ -108,10 +107,11 @@ let p6 = new Produkt('iPad Air', 760, 6, 5);
 let p7 = new Produkt('iPad', 760, 7, 6);
 let p8 = new Produkt('iPad mini', 760, 8, 7);
 
-let sortiment = new Sortiment();
 
-//alle produkte ins sortiment
-sortiment.sortimentHinzufuegen([p1, p2, p2, p3, p4, p5, p6, p7, p8]);
+let sortiment = new Sortiment();    // alle Produkte
+sortiment.hinzufuegen([p1, p2, p3, p4, p5, p6, p7, p8]); //in der Funktion einzeln ins html kommend vom
+sortiment.anzeigen();
 
-//alle produkte des sortiments anzeigen
-sortiment.sortimentAnzeigen();
+let warenkorb = new Warenkorb();    // eingekaufte Produkte
+
+
